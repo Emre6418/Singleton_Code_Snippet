120832c16e55c7f2f07b0e3f3d85dc48777fef47
Emre6418 <emre.ermin6418@gmail.com>
2023-09-16 23:44:33 +0300

    Singleton Code Snippet

---
+++ SingletonCodeSnippet/SingletonCodeSnippet/Models/Singleton.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+
+//The part where I tested the snippet :)
+
+public class Test   
+{
+    private static Test instance;
+
+    private Test()
+    {
+        // Constructor logic here
+    }
+
+    public static Test Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Test();
+            }
+            return instance;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
